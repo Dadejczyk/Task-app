d137f5325560ac0f9f62eef4f353ab0ce974a416
Bartek <b.dadej14@gmail.com>
2024-03-11 20:13:43 +0100

    add

---
+++ src/main/java/com/example/demo/model/projection/ProjectWriteModel.java
@@ -0,0 +1,48 @@
+package com.example.demo.model.projection;
+
+
+import com.example.demo.model.Project;
+import com.example.demo.model.ProjectStep;
+import com.example.demo.model.TaskGroup;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProjectWriteModel {
+    @NotBlank(message = "Project's description must not be empty")
+    private String description;
+    @Valid
+    private List<ProjectStep> steps = new ArrayList<>();
+
+    public ProjectWriteModel() {
+        steps.add(new ProjectStep());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public List<ProjectStep> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(final List<ProjectStep> steps) {
+        this.steps = steps;
+    }
+
+    public Project toProject() {
+        var result = new Project();
+        result.setDescription(description);
+        steps.forEach(step -> step.setProject(result));
+        result.setSteps(new HashSet<>(steps));
+        return result;
+    }
+}
